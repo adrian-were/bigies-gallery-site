02e29aa99ac6cb456b428578b2cf384761fb68f8
Adrian <adrianshisia@gmail.com>
2024-03-29 11:01:57 +0300

    Landing page finished

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+        // <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ position: "sticky", top: "0px" }}>
+//   <div className="container-fluid">
+//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse" id="navbarNav">
+//       <ul className="navbar-nav">
+//         <li className="nav-item">
+//           <a to="/about" className="nav-link active" aria-current="page" href="#">About Us</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="#our-artists">Our Artists</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="#gallery">Gallery</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="#">Our Shop</a>
+//         </li>
+//         <li className="nav-item ">
+//           <a className="nav-link" href="#">Contact Us</a>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <div>
+         <p className='art'>"We're a sanctuary for art enthusiasts, a digital canvas of expression."</p>
+         <div id='out'>
+         <img src="https://img.freepik.com/premium-photo/person-painting-painting-with-paint-brush_918100-32.jpg"  alt='character' />
+         <p className='art-2'>In the world of art, every stroke is a heartbeat, every shade is an emotion, and every masterpiece is a conversation between the artist and the universe</p>
+         </div>
+         <div className='zigzag'></div>
+        </div>
+    )
+}
+
+export default Landing
